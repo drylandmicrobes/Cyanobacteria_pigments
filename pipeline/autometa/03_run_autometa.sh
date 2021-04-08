c91e391e6eb24cc0d7a703238d33d11c0379b633
Jason Stajich <jasonstajich.phd@gmail.com>
2021-04-08 10:32:40 -0700

    streamline for this process, fix num cores, can run on short queue as it is pretty fast

--- pipeline/autometa/03_run_autometa.sh
+++ pipeline/autometa/03_run_autometa.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
-#SBATCH -p batch --mem 16gb -N 1 -n 24 --out autometa_run.%a.log
-
+#SBATCH -p short --mem 16gb -N 1 -n 4 --out logs/autometa_run.%a.%A.log
 
 
 CPU=1
@@ -17,25 +16,21 @@ if [ -z $N ]; then
 	exit
     fi
 fi
-
+AUTOMETAOUT=autometa_runs
+COVERAGE=coverage
 DAT=lib/ncbi_accessions.csv
 IFS=,
 tail -n +2 $DAT | sed -n ${N}p | while read SPECIES STRAIN ACCESSION ASMNAME BIOPROJECT SRA HABITAT LOCATION
 do
     OUTNAME=$(echo "${SPECIES}_$STRAIN" | perl -p -e 's/\s+$//; s/[\s\/\;]/_/g')
     echo "Processing $OUTNAME"
-    OUT=coverage
 #    GENOMEFILE=genomes/${OUTNAME}.dna.fasta
-    COVTAB=$(realpath $OUT/$OUTNAME.coverage.tab)
+    COVTAB=$COVERAGE/$OUTNAME.coverage.tab
 
-    if [ -d $OUTNAME.autometa ]; then
-	pushd $OUTNAME.autometa
-
+    if [ -d $AUTOMETAOUT/$OUTNAME ]; then
 	module load autometa/1.0.2
-	source activate autometa
 	module load git
-
-	run_autometa.py -k bacteria -a Bacteria.fasta --ML_recruitment \
-	    --processors $CPU --length_cutoff 1500 --taxonomy_table taxonomy.tab -o Bacteria_run -v $COVTAB
+	time run_autometa.py -k bacteria -a $AUTOMETAOUT/$OUTNAME/Bacteria.fasta --ML_recruitment \
+		--processors $CPU --length_cutoff 1500 --taxonomy_table $AUTOMETAOUT/$OUTNAME/taxonomy.tab -o $AUTOMETAOUT/$OUTNAME/Bacteria_run -v $COVTAB
     fi
 done
